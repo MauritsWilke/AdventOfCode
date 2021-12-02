7fa748d1e1183e7a308cc172ffa5f859bebfa11f
SadBoisInc <65899425+SadBoisInc@users.noreply.github.com>
2021-12-02 09:46:04 +0100

    Day 2 - First Try again

---
+++ day_2/challenge1.js
@@ -0,0 +1,11 @@
+const fs = require(`fs`)
+const input = (fs.readFileSync("./input.txt", "utf-8")).split("\n");
+
+let horizontalPos = 0;
+let depth = 0;
+for(movement of input){
+    if(movement.includes("forward")) horizontalPos += +movement.replace(/\D/g, "")
+    if(movement.includes("up")) depth -= +movement.replace(/\D/g, "")
+    if(movement.includes("down")) depth += +movement.replace(/\D/g, "")
+}
+console.log(`Horizontal Movement: ${horizontalPos}\nDepth: ${depth}\nMultiplied: ${horizontalPos * depth}`) // 2215080
