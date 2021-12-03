9d2d3e1482361818b6854877a849d61fefd0f336
Maurits Wilke <mauritswilke@gmail.com>
2021-12-03 17:32:27 +0100

    Day 3; First one was doable, second one was a pain in the ass

--- day_2/challenge2.js
+++ day_2/challenge2.js
@@ -1,15 +1,15 @@
 const fs = require(`fs`)
-const input = (fs.readFileSync("./input.txt", "utf-8")).split("\n");
+const input = (fs.readFileSync("./day_2/input.txt", "utf-8")).split("\n");
 
 let horizontalPos = 0;
 let depth = 0;
 let aim = 0;
-for(movement of input){
-    if(movement.includes("forward")){
-        horizontalPos += +movement.replace(/\D/g, "")
-        depth += (aim * +movement.replace(/\D/g, ""))
-    }
-    if(movement.includes("up")) aim -= +movement.replace(/\D/g, "")
-    if(movement.includes("down")) aim += +movement.replace(/\D/g, "")
+for (movement of input) {
+	if (movement.includes("forward")) {
+		horizontalPos += +movement.replace(/\D/g, "")
+		depth += (aim * +movement.replace(/\D/g, ""))
+	}
+	if (movement.includes("up")) aim -= +movement.replace(/\D/g, "")
+	if (movement.includes("down")) aim += +movement.replace(/\D/g, "")
 }
 console.log(`Horizontal Movement: ${horizontalPos}\nDepth: ${depth}\nAim: ${aim}\nMultiplied: ${horizontalPos * depth}`) // 1864715580
---
+++ day_3/challenge2.js
@@ -0,0 +1,37 @@
+const fs = require(`fs`)
+const input = (fs.readFileSync("./day_3/input.txt", "utf-8")).split("\n");
+
+let oxygenArray = input
+for (i = 0; i < 12; i++) {
+	if (oxygenArray.length != 1) {
+		let mostCommonBit;
+		let amountOfOnes = 0;
+		let amountOfZero = 0;
+
+		for (data of oxygenArray) data[i] == 1 ? amountOfOnes++ : amountOfZero++
+
+		if (amountOfOnes == amountOfZero) mostCommonBit = 1;
+		else mostCommonBit = amountOfOnes > amountOfZero ? 1 : 0
+
+		oxygenArray = oxygenArray.filter(data => data[i] == mostCommonBit)
+	}
+}
+console.log(oxygenArray[0]) // 010110101101
+
+let carbonArray = input
+for (i = 0; i < 12; i++) {
+	if (carbonArray.length != 1) {
+		let leastCommonBit;
+		let amountOfOnes = 0;
+		let amountOfZero = 0;
+
+		for (data of carbonArray) data[i] == 1 ? amountOfOnes++ : amountOfZero++
+
+		if (amountOfOnes == amountOfZero) leastCommonBit = 0;
+		else leastCommonBit = amountOfOnes > amountOfZero ? 0 : 1
+		carbonArray = carbonArray.filter(data => data[i] == leastCommonBit)
+	}
+}
+console.log(carbonArray[0]) // 100111110001
+
+console.log(parseInt(oxygenArray[0], 2) * parseInt(carbonArray[0], 2))
