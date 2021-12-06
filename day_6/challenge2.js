97bed70e8049e8c249f4d7092e7666b6f2a206bb
SadBoisInc <65899425+SadBoisInc@users.noreply.github.com>
2021-12-06 10:55:59 +0100

    Day 6, part 2 without rewriting :L

--- day_6/challenge2.js
+++ day_6/challenge2.js
@@ -2,8 +2,8 @@ const fs = require(`fs`)
 const input = (fs.readFileSync("./day_6/input.txt", "utf-8")).split(`,`)
 let fishes = [...Array(9)].map(() => 0)
 for(i = 0; i < input.length; i++) fishes[input[i]]++
-for (i = 0; i < 80; i++) {
+for (i = 0; i < 256; i++) {
 	fishes[7] += fishes[0] 
 	fishes.push(fishes.shift())
 }
-console.log(fishes.reduce((a, b) => a + b, 0)) // 359344
+console.log(fishes.reduce((a, b) => a + b, 0)) // 1629570219571
