c665ff0fa76be0c8634b4e24bbf604b317f8bb4e
Maurits Wilke <mauritswilke@gmail.com>
2022-10-14 11:34:25 +0200

    feat: :sparkles: Day 10!!

---
+++ src/2015/10/part2.ts
@@ -0,0 +1,21 @@
+export default function solution(input:string) {
+	let splitted = input.split("");
+
+	for(let i = 0; i < 50; i++){
+		let last = splitted[0];
+		let amount = 0;
+		let updated = "";
+		splitted.forEach((v,i) => {
+			if(v === last) amount++;
+			else {
+				updated = `${updated}${amount}${last}`;
+				last = v;
+				amount = 1;
+			}
+			if(i ===  splitted.length-1) updated = `${updated}${amount}${last}`;
+		})
+		splitted = updated.split("");
+	}
+
+	return splitted.length;
+}
