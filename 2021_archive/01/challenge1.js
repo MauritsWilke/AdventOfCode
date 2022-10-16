0b4a094a710b09ec44d4348632c4a7bd28f49119
Maurits Wilke <mauritswilke@gmail.com>
2022-10-16 11:32:38 +0200

    feat: :fire: Removed 2021 archives, no one needs those!

--- 2021_archive/01/challenge1.js
+++
@@ -1,5 +0,0 @@
-const fs = require(`fs`)
-const input = (fs.readFileSync("./day_1/input.txt", "utf-8")).split("\n");
-let timesIncreased = 0;
-for (depth in input) if (input[depth] > +input[depth - 1]) timesIncreased++
-console.log(timesIncreased) // 1466
