9a3b44c7e54e657af06042a4c0e317062db45c6f
Maurits Wilke <mauritswilke@gmail.com>
2022-10-17 08:27:45 +0200

    feat: :sparkles: Day 15, dont even look at this file :)

---
+++ src/2015/15/part2.ts
@@ -0,0 +1,39 @@
+export default function solution(input:string) {
+
+	const cookies = input.split("\r\n").map(cookie => {
+		const name = cookie.match(/\w+/g)![0]
+		const [capacity, durability, flavour, texture, calories] = cookie.match(/-*\d+/g)!.map(v => +v)
+		
+		return {
+			capacity: capacity,
+			durability: durability,
+			flavour: flavour,
+			texture: texture,
+			calories: calories
+		}
+	});
+	
+	let bestScore = 0;
+	for(let cookie0 = 0; cookie0 < 100; cookie0++){
+		for(let cookie1 = 0; cookie1 < 100; cookie1++){
+			for(let cookie2 = 0; cookie2 < 100; cookie2++){
+				for(let cookie3 = 0; cookie3 < 100; cookie3++){
+					if(cookie0 + cookie1 + cookie2 + cookie3 !== 100) continue;
+					let capacity = cookies[0].capacity * cookie0 + cookies[1].capacity * cookie1 + cookies[2].capacity * cookie2 + cookies[3].capacity * cookie3;
+					let durability = cookies[0].durability * cookie0 + cookies[1].durability * cookie1 + cookies[2].durability * cookie2 + cookies[3].durability * cookie3;
+					let flavour = cookies[0].flavour * cookie0 + cookies[1].flavour * cookie1 + cookies[2].flavour * cookie2 + cookies[3].flavour * cookie3;
+					let texture = cookies[0].texture * cookie0 + cookies[1].texture * cookie1 + cookies[2].texture * cookie2 + cookies[3].texture * cookie3;
+					let calories = cookies[0].calories * cookie0 + cookies[1].calories * cookie1 + cookies[2].calories * cookie2 + cookies[3].calories * cookie3;
+					
+					if([capacity,durability,flavour,texture].some(v => v <= 0)) continue;
+					if(calories !== 500) continue;
+
+					const totalScore = capacity * durability * flavour * texture;
+					if(totalScore > bestScore) bestScore = totalScore
+				}
+			}
+		}
+	}
+
+	return bestScore;
+}
