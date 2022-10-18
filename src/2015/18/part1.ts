d2ddffbc9f7950a99f410f04847cf8a2d125d456
Maurits Wilke <mauritswilke@gmail.com>
2022-10-18 20:04:52 +0200

    feat: :sparkles: day 18, conway would be crying

---
+++ src/2015/18/part1.ts
@@ -0,0 +1,37 @@
+export default function solution(input:string) {
+	let grid = input.split("\r\n").map(v => v.split(""));
+	grid.forEach(line => {
+		line.push(".");
+		line.unshift(".");
+	});
+
+	// @ts-ignore because TS doesn't like me (understandable)
+	["unshift","push"].forEach(f => grid[f](new Array(grid[0].length).fill(".")));
+
+	for(let it = 0; it < 100; it++){
+		const copy = JSON.parse(JSON.stringify(grid));
+
+		for(let i = 1; i < grid.length - 1; i++){
+			for(let j = 1; j < grid[0].length - 1; j++){
+				let lightsOn = 0;
+				[-1, 0, 1].forEach(v => {
+					[-1, 0, 1].forEach(w => {
+						if(grid[i + v][j + w] === "#" && !(v === 0 && w === 0)) lightsOn++;
+					})
+				});
+
+				if(grid[i][j] === "#"){
+					if(lightsOn === 2 || lightsOn === 3) copy[i][j] = "#";
+					else copy[i][j] = "."
+				} else {
+					if(lightsOn === 3) copy[i][j] = "#"
+				}
+			}
+		}
+
+		grid = [...copy];
+	}
+
+	const lightsOn = grid.map(v => v.filter(v => v === "#").length).reduce((a,b) => a + b, 0);
+	return lightsOn
+}
