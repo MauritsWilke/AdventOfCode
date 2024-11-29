809bbeaca0262848b9a3cb618311f13c4a0a0ed1
MauritsWilke <mauritswilke@gmail.com>
2024-11-29 23:31:53 +0100

    fix: fetch only after local check
    Instead of doing the fetch and then checking...

--- utils/api/get_input.ts
+++ utils/api/get_input.ts
@@ -1,33 +1,29 @@
 export async function fetchInput(YEAR: string, DATE: string, SESSION: string) {
     const formattedDate = `0${DATE}`.slice(-2);
 
-    const res = await fetch(`https://adventofcode.com/${YEAR}/day/${DATE}/input`, {
-        headers: {
-            "Cookie": `session=${SESSION}`,
-            "User-Agent": "https://github.com/MauritsWilke/AdventOfCode by mauritswilke@gmail.com"
-        }
-    });
+    try {
+        await Deno.readFile(`./input/${YEAR}/${formattedDate}/input.txt`)
+        console.log(`%cGET%c File already exists! Using locally cached input instead`, "background-color: orange", "background-color: none")
+    } catch (_e) {
 
-    if (!res.ok) {
-        switch (res.status) {
-            case 404: {
-                throw new Error(`${res.status}: This day is not yet available!`);
+        const res = await fetch(`https://adventofcode.com/${YEAR}/day/${DATE}/input`, {
+            headers: {
+                "Cookie": `session=${SESSION}`,
+                "User-Agent": "https://github.com/MauritsWilke/AdventOfCode by mauritswilke@gmail.com"
             }
-            case 400: {
-                throw new Error(`${res.status}: Bad credentials!`)
-            }
-            default: {
-                throw new Error(`${res.status} ${res.statusText}`);
+        });
+
+        if (!res.ok) {
+            switch (res.status) {
+                case 404: throw new Error(`${res.status}: This day is not yet available!`);
+                case 400: throw new Error(`${res.status}: Bad credentials!`)
+                default: throw new Error(`${res.status} ${res.statusText}`);
             }
         }
+
+        const data = await res.bytes();
+
+        console.log(`%cGET%c | Fetched the input!`, "background-color: green", "background-color: none");
+        Deno.writeFileSync(`./input/${YEAR}/${formattedDate}/input.txt`, data);
     }
-
-    const data = await res.bytes();
-
-    await Deno.readFile(`./input/${YEAR}/${formattedDate}/input.txt`)
-        .then(() => console.log(`%cGET%c File already exists! Using locally cached input instead`, "background-color: orange", "background-color: none"))
-        .catch(_e => {
-            console.log(`%cGET%c | Fetched the input!`, "background-color: green", "background-color: none");
-            Deno.writeFileSync(`./input/${YEAR}/${formattedDate}/input.txt`, data);
-        })
 }
