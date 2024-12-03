9576f9e1d36ccc15660ab927561313960212777e
Maurits Wilke <mauritswilke@gmail.com>
2024-12-03 06:52:45 +0100

    feat: add warnings when solving with weird type

--- utils/solve.ts
+++ utils/solve.ts
@@ -4,7 +4,12 @@ import { run } from "./run.ts";
 const SESSION = Deno.env.get("SESSION")!; // TS-Config disagrees with its existence even if I check it
 const YEAR = Deno.args[0] ?? new Date().getFullYear();
 const DATE = Deno.args[1] ?? new Date().getDate();
-const PART = Deno.args[2] ?? 1
+const PART = Deno.args[2] ?? 1;
 
 const ATTEMPT = await run(YEAR, DATE, PART);
+if (typeof ATTEMPT !== "string" && typeof ATTEMPT !== "number") {
+    console.log(`%c❗ Submitting with "${ATTEMPT}", are you sure?`, "background-color: red");
+    const conf = confirm();
+    if (!conf) Deno.exit();
+}
 await submit(YEAR, DATE, PART, SESSION, ATTEMPT);
