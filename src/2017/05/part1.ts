30263bd78b26871523ebe1fcf9450fdff0ebd585
Maurits Wilke <mauritswilke@gmail.com>
2024-12-05 19:10:33 +0100

    feat: solve day 5

---
+++ src/2017/05/part1.ts
@@ -0,0 +1,18 @@
+export default function solution(input: string) {
+    const offsets = input.lines().map(Number);
+
+    let steps = 0;
+    for (let i = 0; i < offsets.length; steps++) i += offsets[i]++;
+    return steps;
+}
+
+const test_case = `0
+3
+0
+1
+-3
+`
+
+export const tests = [
+    [test_case, 5]
+]
