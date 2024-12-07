1c27ad61e21f4a0bd31e2f5dbe5288ed3604eccf
Maurits Wilke <mauritswilke@gmail.com>
2024-12-07 17:01:53 +0100

    feat: solve day 7

---
+++ src/2024/07/part1.ts
@@ -0,0 +1,36 @@
+export default function solution(input: string) {
+    return input
+        .lines()
+        .map(v => v.split(":").map(v => v.nums()))
+        .filter(v => {
+            const sum = v[0][0];
+            const elms = v[1];
+
+            const result = dfs(elms[0], 0, elms, sum);
+            return result;
+        }).reduce((a, b) => a + b[0][0], 0)
+
+    function dfs(start: number, index: number, elms: number[], target: number): boolean {
+        const next = elms[index + 1];
+        if (start === target) return true;
+        if (next === undefined || start > target) return false
+
+        return dfs(start * next, index + 1, elms, target) ||
+            dfs(start + next, index + 1, elms, target);
+    }
+}
+
+const test_case = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
+
+export const tests = [
+    [test_case, 3749],
+]
